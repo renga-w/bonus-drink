33a8c3930168b07363717ce2c85f4103c14b85b8
renga-w <49421831+renga-w@users.noreply.github.com>
2019-05-04 12:43:31 +0900

    Update bonus_drink.rb

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,12 @@
-class BonusDrink
-  def self.total_count_for(amount)
-    0
-  end
+class BounusDrink
+   @@t=0                  #飲んだ数
+   @@b=0                  #残り数
+   def self.total_count_for(a)     #aは飲める数
+      @@t+=a
+      return @@t if (a+@@b)<3      #３本交換終了
+      n=((a+@@b)/3).to_i           #次に飲める数
+      @@b=(a+@@b)%3                #残り
+  #    print("--#{a}--#{n}--#{@@b}\n")
+      self.total_count_for(n)
+   end
 end
